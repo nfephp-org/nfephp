f84a877e10234d7cfdf6e347198e11aec51b5e09
walkeralencar <walkeralencar@gmail.com>
2009-09-10 05:10:32 +0000

    [UPDATE] - Atualização da Licença.

--- NFeTools/retInutNFe.class.php
+++ NFeTools/retInutNFe.class.php
@@ -1,10 +1,22 @@
 <?php
 /**
- * NFePHP - Nota Fiscal eletrônica em PHP
+ * Este arquivo é parte do projeto NFePHP - Nota Fiscal eletrônica em PHP.
+ *
+ * Este programa é um software livre: você pode redistribuir e/ou modificá-lo
+ * sob os termos da Licença Pública Geral GNU como é publicada pela Fundação 
+ * para o Software Livre, na versão 3 da licença, ou qualquer versão posterior.
+ *
+ * Este programa é distribuído na esperança que será útil, mas SEM NENHUMA
+ * GARANTIA; sem mesmo a garantia explícita do VALOR COMERCIAL ou ADEQUAÇÃO PARA
+ * UM PROPÓSITO EM PARTICULAR, veja a Licença Pública Geral GNU para mais
+ * detalhes.
+ *
+ * Você deve ter recebido uma cópia da Licença Publica GNU junto com este
+ * programa. Caso contrário consulte <http://www.fsfla.org/svnwiki/trad/GPLv3>.
  *
  * @package   NFePHP
  * @name      retInutNFe
- * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL v.3
  * @copyright 2009 &copy; NFePHP
  * @link      http://www.nfephp.org/
  * @author    {@link http://www.walkeralencar.com Walker de Alencar} <contato@walkeralencar.com>
@@ -14,7 +26,7 @@
  * retInutNFe
  *
  * @author  Roberto L. Machado <roberto.machado@superig.com.br>
- * @author  Djalma Fadel Junior <dfadel at ferasoft dot com dot br>
+ * @author  Djalma Fadel Junior <dfadel@ferasoft.com.br>
  */
 class NFeTools_retInutNFe {
     public $versao;     // versao do layout
