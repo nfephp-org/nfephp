82aea1e9c07e403ce448fe9920f56f1f86551d40
robmachado <linux.rlm@gmail.com>
2011-12-23 12:58:27 +0000

    [INC] Inclusão dos arquivos na pasta trunk

---
+++ exemplos/testaEnvio.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * Exemplo de envio de Nfe já assinada e validada
+ */
+require_once('../libs/ToolsNFePHP.class.php');
+$nfe = new ToolsNFePHP;
+$modSOAP = '2'; //usando cURL
+
+//use isso, este é o modo manual voce tem mais controle sobre o que acontece  
+$filename = './35101158716523000119550010000000011003000000-nfe.xml';
+//obter um numero de lote
+$lote = substr(str_replace(',','',number_format(microtime(true)*1000000,0)),0,15);
+// montar o array com a NFe
+$aNFe = array(0=>file_get_contents($filename));
+//enviar o lote
+if ($aResp = $nfe->sendLot($aNFe, $lote, $modSOAP)){
+    if ($aResp['bStat']){
+        echo "Numero do Recibo : " . $aResp['nRec'] .", use este numero para obter o protocolo ou informações de erro no xml com testaRecibo.php.";  
+    } else {
+        echo "Houve erro !! $nfe->errMsg";
+    }
+} else {
+    echo "houve erro !!  $nfe->errMsg";
+}
+echo '<BR><BR><h1>DEBUG DA COMUNICAÇÕO SOAP</h1><BR><BR>';
+echo '<PRE>';
+echo htmlspecialchars($nfe->soapDebug);
+echo '</PRE><BR>';
+
+//ou isso 
+//este é modo interno e vai enviar todoas as nf que estiverem na pasta validadas
+/*
+if ($recibo  = $nfe->autoEnvNFe()){
+    echo "Numero do Recibo : " . $recibo .", use este numero para obter o protocolo ou informações de erro no xml.";  
+} else {
+    echo "Houve erro !! $nfe->errMsg";
+}
+*/
+
+?>
---
+++ exemplos/testaValidadeNFe.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Este é um exemplo para verificação da validade de uma NFe recebida de terceiros
+ * esta NFe deve ter o seu protocolo anexado 
+ * O método irá verificar a aasinatura digital, o protocolo e o digest fornecido pela SEFAZ 
+ * através de uma consulta ao SEFAZ do estado do emissor
+ */
+header('Content-type: text/html; charset=UTF-8');
+require_once('../libs/ToolsNFePHP.class.php');
+$nfe = new ToolsNFePHP;
+//path para o arquivo da NFe recebida de terceiros que se quer verificar a validade
+$fileNFe = "./31110303013973000749550010000222330108804222-nfe.xml";
+if( !$nfe->verifyNFe($fileNFe) ){
+    echo $nfe->errMsg;
+} else {
+    echo "NFe APROVADA e VÁLIDA";
+}
+?>
