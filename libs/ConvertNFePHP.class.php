f40d3ee7a840c5ce748a8cdf7b6f56543d2f5fb6
robmachado <linux.rlm@gmail.com>
2012-06-24 12:54:48 +0000

    [UPD] (TRUNK) ToolsNFePHP.class.php ver.2.3.7 - Inclusão de filtro de correção dos numeros de endereços do destinatário, retirada e entrega, removendo qualquer Letra ou Simbolo e caso seja vazio informa 0 (zero) como o emissor da SEFAZ - Contribuição de Roberto Spadim <rspadim at gmail dot com>

--- libs/ConvertNFePHP.class.php
+++ libs/ConvertNFePHP.class.php
@@ -26,7 +26,7 @@
  *
  * @package     NFePHP
  * @name        ConvertNFePHP
- * @version     2.3.6
+ * @version     2.3.7
  * @license     http://www.gnu.org/licenses/gpl.html GNU/GPL v.3
  * @license     http://www.gnu.org/licenses/lgpl.html GNU/LGPL v.3
  * @copyright   2009-2011 &copy; NFePHP
@@ -543,6 +543,10 @@ class ConvertNFePHP {
                     $enderDest = $dom->createElement("enderDest");
                     $xLgr = $dom->createElement("xLgr", $dados[1]);
                     $enderDest->appendChild($xLgr);
+                    $dados[2] = preg_replace("/[^0-9]/", "", $dados[2]);
+                    if($dados[2]==''){
+                        $dados[2] = 0;
+                    }
                     $nro = $dom->createElement("nro", $dados[2]);
                     $enderDest->appendChild($nro);
                     if(!empty($dados[3])){
@@ -584,6 +588,10 @@ class ConvertNFePHP {
                         $retirada->appendChild($xLgr);
                     }
                     if(!empty($dados[2])) {
+                        $dados[2] = preg_replace("/[^0-9]/", "", $dados[2]);
+                        if($dados[2]==''){
+                            $dados[2] = 0;
+                        }
                         $nro = $dom->createElement("nro", $dados[2]);
                         $retirada->appendChild($nro);
                     }
@@ -632,6 +640,10 @@ class ConvertNFePHP {
                         $entrega->appendChild($xLgr);
                     }
                     if(!empty($dados[2])) {
+                        $dados[2] = preg_replace("/[^0-9]/", "", $dados[2]);
+                        if($dados[2]==''){
+                            $dados[2] = 0;
+                        }
                         $nro = $dom->createElement("nro", $dados[2]);
                         $entrega->appendChild($nro);
                     }
