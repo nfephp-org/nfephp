d5ffca560058227cb4b029557357a9e01d64743b
robmachado <linux.rlm@gmail.com>
2012-09-01 12:53:41 +0000

    [INC] Inclusão de arquivos relativos a MDFe

---
+++ libs/MDFeNFePHP.class.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Este arquivo é parte do projeto NFePHP - Nota Fiscal eletrônica em PHP.
+ *
+ * Este programa é um software livre: você pode redistribuir e/ou modificá-lo
+ * sob os termos da Licença Pública Geral GNU (GPL)como é publicada pela Fundação
+ * para o Software Livre, na versão 3 da licença, ou qualquer versão posterior
+ * e/ou 
+ * sob os termos da Licença Pública Geral Menor GNU (LGPL) como é publicada pela Fundação
+ * para o Software Livre, na versão 3 da licença, ou qualquer versão posterior.
+ *
+ *
+ * Este programa é distribuído na esperança que será útil, mas SEM NENHUMA
+ * GARANTIA; nem mesmo a garantia explícita definida por qualquer VALOR COMERCIAL
+ * ou de ADEQUAÇÃO PARA UM PROPÓSITO EM PARTICULAR,
+ * veja a Licença Pública Geral GNU para mais detalhes.
+ *
+ * Você deve ter recebido uma cópia da Licença Publica GNU e da 
+ * Licença Pública Geral Menor GNU (LGPL) junto com este programa.
+ * Caso contrário consulte <http://www.fsfla.org/svnwiki/trad/GPLv3> ou
+ * <http://www.fsfla.org/svnwiki/trad/LGPLv3>. 
+ *
+ * Está atualizada para :
+ *      PHP 5.3
+ *      Versão 2 dos webservices da SEFAZ com comunicação via SOAP 1.2
+ *      e conforme Manual de Integração Versão 5
+ *
+ * Atenção: Esta classe não mantêm a compatibilidade com a versão 1.10 da SEFAZ !!!
+ *
+ * @package   NFePHP
+ * @name      MDFeNFePHP
+ * @version   0.1.0
+ * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL v.3
+ * @copyright 2009-2012 &copy; NFePHP
+ * @link      http://www.nfephp.org/
+ * @author    Roberto L. Machado <linux.rlm at gmail dot com>
+ *
+ *        CONTRIBUIDORES (em ordem alfabetica):
+ *
+ *
+ */
+//define o caminho base da instalação do sistema
+if (!defined('PATH_ROOT')) {
+   define('PATH_ROOT', dirname(dirname( __FILE__ )) . DIRECTORY_SEPARATOR);
+}
+/**
+ * Classe principal "CORE class"
+ */
+class MDFePHP {
+    
+}
+?>
