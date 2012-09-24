3a3159a9342cc52c5c1d12f0470bcc3671b9c620
robmachado <linux.rlm@gmail.com>
2012-09-24 10:46:55 +0000

    [UPD] Correção do nome da classe para impressão em pdf ca CCe

--- exemplos/printCCe.php
+++ exemplos/printCCe.php
@@ -1,10 +1,10 @@
 <?php
-require_once('../libs/CCeNFePHP.class.php');
+require_once('../libs/DacceNFePHP.class.php');
 
 $arq = './cce.xml';
 $aEnd = array('logradouro' => 'AV. ATLANTICA','numero' => '1702','complemento' => '','bairro' => 'COPACABANA','CEP' => '22021001','municipio' => 'RIO DE JANEIRO','UF'=>'RJ','telefone'=>'2100000000','email'=>'copa@copapalace.com.br');
 if ( is_file($arq) ){
-    $cce = new CCeNFePHP($arq, 'P', 'A4','../images/logo.jpg','I',$aEnd,'','Times',1);
+    $cce = new DacceNFePHP($arq, 'P', 'A4','../images/logo.jpg','I',$aEnd,'','Times',1);
     $teste = $cce->printCCe('teste.pdf','I');
 }
 ?>
