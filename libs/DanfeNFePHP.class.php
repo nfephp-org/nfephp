02c826b1c31b7c25ed409dd749976e0f4a8b3518
robmachado <linux.rlm@gmail.com>
2012-11-15 11:41:38 -0200

    [UPD] DanfeNFePHP.class.php ver. 2.1.18 - Inclusão do cStat 135 para indicação de cancelamento

--- libs/DanfeNFePHP.class.php
+++ libs/DanfeNFePHP.class.php
@@ -23,7 +23,7 @@
  *
  * @package     NFePHP
  * @name        DanfeNFePHP.class.php
- * @version     2.1.17
+ * @version     2.1.18
  * @license     http://www.gnu.org/licenses/gpl.html GNU/GPL v.3
  * @license     http://www.gnu.org/licenses/lgpl.html GNU/LGPL v.3
  * @copyright   2009-2012 &copy; NFePHP
@@ -91,7 +91,7 @@ class DanfeNFePHP extends CommonNFePHP implements DocumentoNFePHP {
     protected $destino = 'I'; //destivo do arquivo pdf I-borwser, S-retorna o arquivo, D-força download, F-salva em arquivo local
     protected $pdfDir=''; //diretorio para salvar o pdf com a opção de destino = F
     protected $fontePadrao='Times'; //Nome da Fonte para gerar o DANFE
-    protected $version = '2.1.17';
+    protected $version = '2.1.18';
     protected $textoAdic = '';
     protected $wAdic = 0;
     protected $wPrint; //largura imprimivel
@@ -999,7 +999,7 @@ class DanfeNFePHP extends CommonNFePHP implements DocumentoNFePHP {
         //Indicação de NF Homologação, cancelamento e falta de protocolo
         $tpAmb = $this->ide->getElementsByTagName('tpAmb')->item(0)->nodeValue;
         //indicar cancelamento
-        if ( $cStat == '101' || $situacao_externa==NFEPHP_SITUACAO_EXTERNA_CANCELADA) {
+        if ( $cStat == '101' || $cStat == '135' || $situacao_externa==NFEPHP_SITUACAO_EXTERNA_CANCELADA) {
             //101 Cancelamento
             $x = 10;
             $y = $this->hPrint-130;
