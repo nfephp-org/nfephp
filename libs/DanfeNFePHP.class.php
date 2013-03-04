f845a170a8b7952c47afe29ace4ae6acbbdc814d
robmachado <linux.rlm@gmail.com>
2013-03-04 08:02:45 -0300

    [UPD] DanfeNFePHP.class.php ver.2.1.21 - Correção de Notice do php, devido a não instanciação de variável de classe, situacao_externa. - Contribuição de Fernando Mertins

--- libs/DanfeNFePHP.class.php
+++ libs/DanfeNFePHP.class.php
@@ -23,7 +23,7 @@
  *
  * @package     NFePHP
  * @name        DanfeNFePHP.class.php
- * @version     2.1.20
+ * @version     2.1.21
  * @license     http://www.gnu.org/licenses/gpl.html GNU/GPL v.3
  * @license     http://www.gnu.org/licenses/lgpl.html GNU/LGPL v.3
  * @copyright   2009-2012 &copy; NFePHP
@@ -80,6 +80,7 @@ class DanfeNFePHP extends CommonNFePHP implements DocumentoNFePHP {
     //publicas
     public $logoAlign='C'; //alinhamento do logo
     public $yDados=0;
+	public $situacao_externa=0;
     //privadas
     protected $pdf; // objeto fpdf()
     protected $xml; // string XML NFe
@@ -91,7 +92,7 @@ class DanfeNFePHP extends CommonNFePHP implements DocumentoNFePHP {
     protected $destino = 'I'; //destivo do arquivo pdf I-borwser, S-retorna o arquivo, D-força download, F-salva em arquivo local
     protected $pdfDir=''; //diretorio para salvar o pdf com a opção de destino = F
     protected $fontePadrao='Times'; //Nome da Fonte para gerar o DANFE
-    protected $version = '2.1.18';
+    protected $version = '2.1.21';
     protected $textoAdic = '';
     protected $wAdic = 0;
     protected $wPrint; //largura imprimivel
