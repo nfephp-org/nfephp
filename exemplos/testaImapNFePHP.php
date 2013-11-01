9a11702b673299c56a1f3e9c6165fa6753ce843d
Roberto L. Machado <linux.rlm@gmail.com>
2013-11-01 20:28:22 -0200

    [UPD] ImapNFePHP.class.php - Ajustes

--- exemplos/testaImapNFePHP.php
+++ exemplos/testaImapNFePHP.php
@@ -6,23 +6,24 @@ require_once('../libs/ImapNFePHP.class.php');
 
 $mbox = new ImapNFePHP;
 
-$mbox->setHost('imap.gmail.com');
+$mbox->setHost('imap.gamil.com');
 $mbox->setPort('993');
 $mbox->setProtocol('imap');
 $mbox->setSecurity('ssl');
 $mbox->setValidCerts('novalidate-cert');
 $mbox->setImapfolder('INBOX');
 $mbox->setDownfolder('../exemplos');
-$mbox->setUser('user');
-$mbox->setPass('pass');
-
-$mbox->imapConnect();
-
-if ($mbox->imapGetMessages()) {
-    echo 'Baixadas';
-} else {
-    echo 'Error';
+$mbox->setUser('seunome@gmail.com');
+$mbox->setPass('passW0rd');
+if (!$mbox->imapConnect()) {
+    echo $mbox->getImapError().'<br>';
+    echo $mbox->getMbox();
+    exit();
 }
-
+if ($mbox->imapGetXmlFiles($retorno)) {
+    echo 'Mensagens processadas';
+} else {
+    echo 'Error'.$mbox->getImapError().'<br>';
+}
 $mbox->imapDisconnect();
 $mbox = null;
