bdc09a4fef50c2de4a7552029adff5fa3c388569
robmachado <linux.rlm@gmail.com>
2014-04-11 07:19:16 -0300

    Revert "[UPD] Inclusão das novas opções de contingência SVC-AN e SVC-RS"
    This reverts commit bb43600c35f9d3274f6e41d1905fc2af841c6cdf.

--- exemplos/testaNFeStatus.php
+++ exemplos/testaNFeStatus.php
@@ -4,35 +4,26 @@ require_once('../libs/ToolsNFePHP.class.php');
 $nfe = new ToolsNFePHP;
 header('Content-type: text/html; charset=UTF-8');
 $sUF = 'AC;AL;AM;AP;BA;CE;DF;ES;GO;MA;MG;MS;MT;PA;PB;PE;PI;PR;RJ;RN;RO;RR;RS;SC;SE;SP;TO';
-$sUF = 'SP'; //falha ao utilizar PR com SOAP nativo
+//$sUF = 'PR'; //falha ao utilizar PR com SOAP nativo
 
 //determina o tipo de conector 1-SOAP ou 2-cURL
 $modSOAP = '2';
 //determina o ambiente 1-produção 2-homologação
 $tpAmb= '2';
-
-//habilita uso da contingência
+//habilita uso do scan
 //$nfe->enableSCAN = true;
-//$nfe->enableSVCAN = true;
-//$nfe->enableSVCRS = true;
 
-$aUF = explode(';', $sUF);
-if ($tpAmb == 1) {
+$aUF = explode(';',$sUF);
+
+if($tpAmb == 1){
     $sAmb='Produção';
 } else {
     $sAmb='Homologação';
 }
-if ($nfe->enableSCAN) {
-    $cont = ' Contingencia SCAN ';
-} elseif ($nfe->enableSVCAN) {
-    $cont = ' Contingencia SVC-AN ';
-} elseif ($nfe->enableSVCRS) {
-    $cont = ' Contingencia SVC-RS ';
-}
-foreach ($aUF as $UF) {
+foreach ($aUF as $UF){
     echo '<BR><HR/><BR>';
-    echo $UF . '[' . $sAmb . ''.$cont.'] - modSOAP = ' . $modSOAP  . '<BR>';
-    $resp = $nfe->statusServico($UF, $tpAmb, $modSOAP);
+    echo $UF . '[' . $sAmb . '] - modSOAP = ' . $modSOAP  . '<BR>';
+    $resp = $nfe->statusServico($UF,$tpAmb,$modSOAP);
     
     echo print_r($resp);
     echo '<BR>';
@@ -43,3 +34,4 @@ foreach ($aUF as $UF) {
     echo $UF . '[' . $sAmb . '] - ' . $resp['xMotivo'] . '<BR><BR><HR/><BR>';
     flush();
 }
+?>
