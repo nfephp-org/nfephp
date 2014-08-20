33f74eb44331099a8239b3f148ca2be58918d6a4
Marcos Diez <marcos@unitron.com.br>
2014-08-20 17:49:51 +0000

    melhorei testaNFeValidade.php

--- exemplos/testaNFeValidade.php
+++ exemplos/testaNFeValidade.php
@@ -5,14 +5,37 @@
  * O método irá verificar a aasinatura digital, o protocolo e o digest fornecido pela SEFAZ
  * através de uma consulta ao SEFAZ do estado do emissor
  */
+
+function e($number, $msg, $file, $line, $vars) {
+   print_r(debug_backtrace());
+   die();
+}
+set_error_handler('e');
+
+function verifica($arquivo){
+    echo "---INICIANDO VERIFICACAO DE [$arquivo]\n";
+    $nfe = new ToolsNFePHP;
+    $result = $nfe->verifyNFe($arquivo);
+    echo "--FUNCAO EXECUTADA: [$result]\n";
+    if( !$result ){
+        echo "ERROR: [" . trim($nfe->errMsg) . "]\n";
+    } else {
+        echo "NFe APROVADA e VÁLIDA";
+    }
+    $nfe->errMsg="";
+    echo "---FIM DA VERIFICACAO DE [$arquivo]\n";
+}
+
+
 header('Content-type: text/html; charset=UTF-8');
 require_once('../libs/ToolsNFePHP.class.php');
-$nfe = new ToolsNFePHP;
 //path para o arquivo da NFe recebida de terceiros que se quer verificar a validade
-$fileNFe = "xml/11101284613439000180550010000004881093997017-nfe.xml";
-if( !$nfe->verifyNFe($fileNFe) ){
-    echo $nfe->errMsg;
-} else {
-    echo "NFe APROVADA e VÁLIDA";
-}
+
+verifica("xml/nao_existe.xml");
+verifica("xml/assinatura_invalida.xml");
+verifica("xml/digest_invalido.xml");
+verifica("xml/11101284613439000180550010000004881093997017-nfe.xml");
+
+echo "FIM\n";
+
 ?>
