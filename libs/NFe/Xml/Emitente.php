d75e1329a43bf134859c8a9095df1686ba63f79a
robmachado <linux.rlm@gmail.com>
2014-10-17 11:21:38 -0300

    [INC] Inclusão de subclasse NFe\Xml\Emitente.php e Base.php para construir o xml da NFe

---
+++ libs/NFe/Xml/Emitente.php
@@ -0,0 +1,480 @@
+<?php
+
+//namespace NFe\Xml;
+
+//use NFe\Xml\Base;
+//use \DOMDocument;
+
+require_once('./Base.php');
+
+class Emitente extends Base
+{
+    public $cnpj = '';
+    public $cpf = '';
+    public $xNome = '';
+    public $xFant = '';
+    public $numIE = '';
+    public $numIEST = '';
+    public $numIM = '';
+    public $cnae = '';
+    public $crt = '';
+    public $xLgr = '';
+    public $nro = '';
+    public $xCpl = '';
+    public $xBairro = '';
+    public $cMun = '';
+    public $xMun = '';
+    public $siglaUF = '';
+    public $cep = '';
+    public $cPais = '';
+    public $xPais = '';
+    public $fone = '';
+    public $emit; //DOMElement
+
+    private $enderEmit; //DOMElement
+    
+    /**
+     * __contruct
+     * Função construtora cria um objeto DOMDocument
+     * 
+     * @return none
+     */
+    public function __construct($dom = '')
+    {
+        parent::__construct($dom);
+    }
+    
+    /**
+     * setCNPJ
+     * Ajusta da propriedade com o número do CNPJ do emitente, se houver
+     * 
+     * @param string $cnpj
+     */
+    public function setCNPJ($cnpj = '')
+    {
+        if (!empty($cnpj)) {
+            $this->cnpj = $cnpj;
+        }
+    }
+    
+    /**
+     * setCPF
+     * Ajusta a propriedade com o número do CPF, se houver (somente produtores rurais)
+     * 
+     * @param string $cpf
+     */
+    public function setCPF($cpf = '')
+    {
+        if (!empty($cpf)) {
+            $this->cpf = $cpf;
+        }
+    }
+    
+    /**
+     * setNome
+     * Ajusta a propriedade xNome, com a Razão Social da Empresa emitente
+     * 
+     * @param string $xNome
+     */
+    public function setNome($xNome = '')
+    {
+        if (!empty($xNome)) {
+            $this->xNome = $xNome;
+        }
+    }
+    
+    /**
+     * setFantasia
+     * Ajusta a propriedade xFant, com o nome Fantasia (simplificação) do emitente
+     * 
+     * @param string $xFant
+     */
+    public function setFantasia($xFant = '')
+    {
+        if (!empty($xFant)) {
+            $this->xFant = $xFant;
+        }
+    }
+    
+    /**
+     * setIE
+     * Ajusta a propriedade numIE, com o número da Inscrição Estadual
+     * 
+     * @param string $numIE
+     */
+    public function setIE($numIE = '')
+    {
+        if (!empty($numIE)) {
+            $this->numIE = $numIE;
+        }
+    }
+    
+    /**
+     * setIEST
+     * Ajusta a propriedade numIEST, com o numero da inscr. estadual do substituto tributário
+     * 
+     * @param string $numIEST
+     */
+    public function setIEST($numIEST = '')
+    {
+        if (!empty($numIEST)) {
+            $this->numIEST = $numIEST;
+        }
+    }
+    
+    /**
+     * setIM
+     * Ajusta a propriedade numIM, com o numero da inscrição municipal, se houver
+     * 
+     * @param string $numIM
+     */
+    public function setIM($numIM = '')
+    {
+        if (!empty($numIM)) {
+            $this->numIM = $numIM;
+        }
+    }
+    
+    /**
+     * setCNAE
+     * Ajusta a propriedade CNAE, código nacional de atividade econômica
+     * 
+     * @param type $cnae
+     */
+    public function setCNAE($cnae = '')
+    {
+        if (!empty($cnae)) {
+            $this->cnae = $cnae;
+        }
+    }
+    
+    /**
+     * setCRT
+     * Ajusta a propriedade CRT, Código do Regime Tributário
+     * 
+     * @param string $crt
+     */
+    public function setCRT($crt = '')
+    {
+        if (!empty($crt)) {
+            $this->crt = $crt;
+        }
+    }
+    
+    /**
+     * setLogradouro
+     * Ajusta a propriedade xLgr, com o nome da Rua do endereço do emitente
+     * 
+     * @param string $xLgr
+     */
+    public function setLogradouro($xLgr = '')
+    {
+        if (!empty($xLgr)) {
+            $this->xLgr = $xLgr;
+        }
+    }
+    
+    /**
+     * setNumero
+     * Ajusta a propriedade nro, com o numero do endereço
+     * 
+     * @param string $nro
+     */
+    public function setNumero($nro = '')
+    {
+        if (!empty($nro)) {
+            $this->nro = $nro;
+        }
+    }
+    
+    /**
+     * setComplemento
+     * Ajusta a propriedade xCpl, com o complemento do endereço
+     * 
+     * @param string $xCpl
+     */
+    public function setComplemento($xCpl = '')
+    {
+        if (!empty($xCpl)) {
+            $this->xCpl = $xCpl;
+        }
+    }
+    
+    /**
+     * setBairro
+     * Ajusta a propriedade xBairro, com o nome do bairro do endereço
+     * 
+     * @param string $xBairro
+     */
+    public function setBairro($xBairro = '')
+    {
+        if (!empty($xBairro)) {
+            $this->xBairro = $xBairro;
+        }
+    }
+    
+    /**
+     * setCodMun
+     * Ajusta a propriedade cMun, com o código do municipio do IBGE
+     * 
+     * @param string $cMun
+     */
+    public function setCodMun($cMun = '')
+    {
+        if (!empty($cMun)) {
+            $this->cMun = $cMun;
+        }
+    }
+    
+    /**
+     * setMunicipio
+     * Ajusta a propriedade xMun, com o nome do município do endereço
+     * 
+     * @param string $xMun
+     */
+    public function setMunicipio($xMun = '')
+    {
+        if (!empty($xMun)) {
+            $this->xMun = $xMun;
+        }
+    }
+    
+    /**
+     * setUF
+     * Ajusta a propriedade siglaUF, com a sigla do estado do endereço
+     * 
+     * @param string $siglaUF
+     */
+    public function setUF($siglaUF = '')
+    {
+        if (!empty($siglaUF)) {
+            $this->siglaUF = $siglaUF;
+        }
+    }
+    
+    /**
+     * setCEP
+     * Ajusta a propriedade CEP, com o numero do Codigo de endereçamento postal
+     * 
+     * @param string $cep
+     */
+    public function setCEP($cep = '')
+    {
+        if (!empty($cep)) {
+            $this->cep = $cep;
+        }
+    }
+    
+    /**
+     * setCodPais
+     * Ajusta a propriedade cPais, com o código do pais do BACEN
+     * 
+     * @param string $cPais
+     */
+    public function setCodPais($cPais = '')
+    {
+        if (!empty($cPais)) {
+            $this->cPais = $cPais;
+        }
+    }
+    
+    /**
+     * setPais
+     * Ajusta a propriedade xPais, com o nome do Pais do endereço
+     * 
+     * @param string $xPais
+     */
+    public function setNomePais($xPais = '')
+    {
+        if (!empty($xPais)) {
+            $this->xPais = $xPais;
+        }
+    }
+    
+    /**
+     * setFone
+     * Ajusta a propriedade fone, com o numero do telefone do endereço
+     * 
+     * @param string $fone
+     */
+    public function setFone($fone = '')
+    {
+        if (!empty($fone)) {
+            $this->fone = $fone;
+        }
+    }
+    
+    /**
+     * getEmitTag
+     * Monta e retorna a tag emit da NFe, com os dados passados por parametros
+     * para a classe
+     * 
+     * @return DOMElement
+     */
+    public function getEmitTag()
+    {
+        $this->pTagemit();
+        $this->pTagenderEmit();
+        $node = $this->emit->getElementsByTagName("IE")->item(0);
+        $this->emit->insertBefore($this->enderEmit, $node);
+        return $this->emit;
+    }
+    
+    /**
+     * pTagemit
+     * Identificação do emitente da NF-e C01 pai A01
+     * tag NFe/infNFe/emit
+     * @return none
+     */
+    private function pTagemit()
+    {
+        if (!empty($this->emit)) {
+            $this->emit = null;
+        }
+        $identificador = 'C01 <emit> - ';
+        $this->emit = $this->dom->createElement("emit");
+        if ($cnpj != '') {
+            $this->zAddChild($this->emit, "CNPJ", $this->cnpj, true, $identificador . "CNPJ do emitente");
+        } else {
+            $this->zAddChild($this->emit, "CPF", $this->cpf, true, $identificador . "CPF do remetente");
+        }
+        $this->zAddChild(
+            $this->emit,
+            "xNome",
+            $this->xNome,
+            true,
+            $identificador . "Razão Social ou Nome do emitente"
+        );
+        $this->zAddChild(
+            $this->emit,
+            "xFant",
+            $this->xFant,
+            false,
+            $identificador . "Nome fantasia do emitente"
+        );
+        $this->zAddChild(
+            $this->emit,
+            "IE",
+            $this->numIE,
+            true,
+            $identificador . "Inscrição Estadual do emitente"
+        );
+        $this->zAddChild(
+            $this->emit,
+            "IEST",
+            $this->numIEST,
+            false,
+            $identificador . "IE do Substituto Tributário do emitente"
+        );
+        $this->zAddChild(
+            $this->emit,
+            "IM",
+            $this->numIM,
+            false,
+            $identificador . "Inscrição Municipal do Prestador de Serviço do emitente"
+        );
+        $this->zAddChild(
+            $this->emit,
+            "CNAE",
+            $this->cnae,
+            false,
+            $identificador . "CNAE fiscal do emitente"
+        );
+        $this->zAddChild(
+            $this->emit,
+            "CRT",
+            $this->crt,
+            true,
+            $identificador . "Código de Regime Tributário do emitente"
+        );
+    }
+    
+    /**
+     * pTagenderEmit
+     * Endereço do emitente C05 pai C01
+     * tag NFe/infNFe/emit/endEmit
+     * @return none
+     */
+    private function pTagenderEmit()
+    {
+        $identificador = 'C05 <enderEmit> - ';
+        $this->enderEmit = $this->dom->createElement("enderEmit");
+        $this->zAddChild(
+            $this->enderEmit,
+            "xLgr",
+            $this->xLgr,
+            true,
+            $identificador . "Logradouro do Endereço do emitente"
+        );
+        $this->zAddChild(
+            $this->enderEmit,
+            "nro",
+            $this->nro,
+            true,
+            $identificador . "Número do Endereço do emitente"
+        );
+        $this->zAddChild(
+            $this->enderEmit,
+            "xCpl",
+            $this->xCpl,
+            false,
+            $identificador . "Complemento do Endereço do emitente"
+        );
+        $this->zAddChild(
+            $this->enderEmit,
+            "xBairro",
+            $this->xBairro,
+            true,
+            $identificador . "Bairro do Endereço do emitente"
+        );
+        $this->zAddChild(
+            $this->enderEmit,
+            "cMun",
+            $this->cMun,
+            true,
+            $identificador . "Código do município do Endereço do emitente"
+        );
+        $this->zAddChild(
+            $this->enderEmit,
+            "xMun",
+            $this->xMun,
+            true,
+            $identificador . "Nome do município do Endereço do emitente"
+        );
+        $this->zAddChild(
+            $this->enderEmit,
+            "UF",
+            $this->siglaUF,
+            true,
+            $identificador . "Sigla da UF do Endereço do emitente"
+        );
+        $this->zAddChild(
+            $this->enderEmit,
+            "CEP",
+            $this->cep,
+            true,
+            $identificador . "Código do CEP do Endereço do emitente"
+        );
+        $this->zAddChild(
+            $this->enderEmit,
+            "cPais",
+            $this->cPais,
+            false,
+            $identificador . "Código do País do Endereço do emitente"
+        );
+        $this->zAddChild(
+            $this->enderEmit,
+            "xPais",
+            $this->xPais,
+            false,
+            $identificador . "Nome do País do Endereço do emitente"
+        );
+        $this->zAddChild(
+            $this->enderEmit,
+            "fone",
+            $this->fone,
+            false,
+            $identificador . "Telefone do Endereço do emitente"
+        );
+    }
+}
