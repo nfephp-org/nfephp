15d6572e559ee12231bd7c22e1389b1151355550
robmachado <linux.rlm@gmail.com>
2015-01-06 16:39:33 -0200

    [UPD] exemplos\NFe\testaNFeGetList2.php - Correções do exemplo

--- exemplos/NFe/testaNFeGetList2.php
+++ exemplos/NFe/testaNFeGetList2.php
@@ -9,42 +9,43 @@
 //É recomendável que o usuário não tenha acesso a essa chamada e que a mesma seja
 //feita periódicamente (usando um agendador) por exemplo 3 ou 4 vezes ao dia no 
 //máximo.
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
 
 require_once('../../libs/NFe/ToolsNFePHP.class.php');
-$nfe = new ToolsNFePHP('',1,false);
+$nfe = new ToolsNFePHP('', 1, false);
 $modSOAP = '2'; //usando cURL
 $tpAmb = '1';//usando produção
 $indNFe = '0';
 $indEmi = '0';
 $ultNSU = '';
-$AN = true;
+$ambNac = true;
 $retorno = array();
 $indCont = 1;
 $limite = 1;
 $nxm = '<?xml version="1.0" encoding="utf-8"?><root>';
-while ($indCont != 0) { 
-    if (!$xml = $nfe->getListNFe($AN, $indNFe, $indEmi, $ultNSU, $tpAmb, $modSOAP, $retorno)) {
+while ($indCont != 0) {
+    if (!$xml = $nfe->getListNFe($ambNac, $indNFe, $indEmi, $ultNSU, $tpAmb, $retorno)) {
         echo "Houve erro !! $nfe->errMsg";
         echo '<br><br><PRE>';
         echo htmlspecialchars($nfe->soapDebug);
         echo '</PRE><BR>';
         exit;
-    } else {
-        //carrega o retorno 
-        $indCont = $retorno['indCont'];
-        $nxm .= '<pesquisa num="'.$limite.'">';
-        $nxm .= str_replace('<?xml version="1.0" encoding="utf-8"?>', '', $xml);
-        $nxm .= '</pesquisa>';
     }
+    //carrega o retorno
+    $indCont = $retorno['indCont'];
+    $nxm .= '<pesquisa num="'.$limite.'">';
+    $nxm .= str_replace('<?xml version="1.0" encoding="utf-8"?>', '', $xml);
+    $nxm .= '</pesquisa>';
     $limite++;
-    //atençao o tempo de execução desse loop pode exceder 
+    //atençao o tempo de execução desse loop pode exceder
     //o tempo limite de processamento do php e o script pode ser interrompido
     //é recomendável que a pesquisa seja feita em etapas usando o numero do ultNSU
-    //registrado 
-    if ($limite > 130) {
+    //registrado
+    if ($limite > 30) {
         break;
     }
-    //tem de haver um intervalo de tempo entre cada pesquisa caso contrario o 
+    //tem de haver um intervalo de tempo entre cada pesquisa caso contrario o
     //webservice pode parar de responder, considerando ou um excesso de consultas
     //ou um ataque DoS
     sleep(5);
