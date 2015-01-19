519702bfdae36caf3739989e2d5ae4eaa057d6e3
Fabio Migliorini <fabio.migliorini@gmail.com>
2015-01-18 22:49:36 -0200

    [UPD] Correcao path LIBS

--- libs/NFe/DanfeNFCeNFePHP.class.php
+++ libs/NFe/DanfeNFCeNFePHP.class.php
@@ -49,10 +49,10 @@ if (!defined('PATH_ROOT')) {
 set_time_limit(1800);
 
 //classes utilizadas
-require_once PATH_ROOT.'libs/External/mpdf/mpdf.php';
-require_once PATH_ROOT.'libs/External/qrcode/qrcode.class.php';
-require_once PATH_ROOT.'libs/Common/CommonNFePHP.class.php';
-require_once PATH_ROOT.'libs/Common/DocumentoNFePHP.interface.php';
+require_once PATH_ROOT.'External/mpdf/mpdf.php';
+require_once PATH_ROOT.'External/qrcode/qrcode.class.php';
+require_once PATH_ROOT.'Common/CommonNFePHP.class.php';
+require_once PATH_ROOT.'Common/DocumentoNFePHP.interface.php';
 
 
 
