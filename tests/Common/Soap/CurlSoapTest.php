f60c016759f669158997a927c2474dd199c9b889
marabesi <matheus.marabesi@gmail.com>
2015-03-21 16:30:23 -0300

    Adicionado novos testes para aumentar a cobertura da classe CurlSoap

--- tests/Common/Soap/CurlSoapTest.php
+++ tests/Common/Soap/CurlSoapTest.php
@@ -34,6 +34,24 @@ class CurlSoapTest extends PHPUnit_Framework_TestCase
     
     /**
      * @expectedException Common\Exception\InvalidArgumentException
+     * @expectedExceptionMessage O protocolo SSL pode estar entre 0 e seis, inclusive, mas não além desses números.
+     */
+    public function testExceptionAoPassarProtocoloSslMenorQueZero()
+    {
+        $soap = new CurlSoap('', '', '', '', -2);
+    }
+    
+    /**
+     * @expectedException Common\Exception\InvalidArgumentException
+     * @expectedExceptionMessage O protocolo SSL pode estar entre 0 e seis, inclusive, mas não além desses números.
+     */
+    public function testExceptionAoPassarProtocoloSslMaiorQueSeis()
+    {
+        $soap = new CurlSoap('', '', '', '', 7);
+    }
+    
+    /**
+     * @expectedException Common\Exception\InvalidArgumentException
      * @expectedExceptionMessage Alguns dos certificados não foram encontrados ou o timeout pode não ser numérico.
      */
     public function testExceptionAoPassarCertificados()
@@ -71,6 +89,29 @@ class CurlSoapTest extends PHPUnit_Framework_TestCase
         $wsdlstd = file_get_contents($wsdlfile);
         //teste
         $this->assertEquals($wsdl, $wsdlstd);
+    }
+    
+    public function testErroGetWsdl()
+    {
+        $priKey = dirname(dirname(dirname(__FILE__))) . '/fixtures/certs/99999090910270_priKEY.pem';
+        $pubKey = dirname(dirname(dirname(__FILE__))) . '/fixtures/certs/99999090910270_pubKEY.pem';
+        $certKey = dirname(dirname(dirname(__FILE__))) . '/fixtures/certs/99999090910270_certKEY.pem';
+        $timeout = '10';
+        $args = array($priKey, $pubKey, $certKey, $timeout);
+        //cria uma função "FAJUTA" de comunicação com a SEFAZ
+        $soap = $this->getMockBuilder('Common\Soap\CurlSoap')
+            ->setConstructorArgs($args)
+            ->setMethods(array('zCommCurl'))
+            ->getMock();
+
+        $soap->expects($this->once())
+                ->method('zCommCurl')
+                ->will($this->returnValue(false));
+        //busca resposta
+        $urlservice = 'https://homologacao.nfe.fazenda.sp.gov.br/ws/nfestatusservico2.asmx';
+        $wsdl = $soap->getWsdl($urlservice);
+        
+        $this->assertFalse($wsdl);
     }
     
     /**
@@ -139,5 +180,34 @@ class CurlSoapTest extends PHPUnit_Framework_TestCase
         $respStd = '<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"><soap:Header><nfeCabecMsg xmlns="http://www.portalfiscal.inf.br/nfe/wsdl/NfeStatusServico2"><cUF>35</cUF><versaoDados>3.10</versaoDados></nfeCabecMsg></soap:Header><soap:Body><nfeStatusServicoNF2Result xmlns="http://www.portalfiscal.inf.br/nfe/wsdl/NfeStatusServico2"><retConsStatServ versao="3.10" xmlns="http://www.portalfiscal.inf.br/nfe"><tpAmb>2</tpAmb><verAplic>SP_NFE_PL_008d</verAplic><cStat>107</cStat><xMotivo>Serviço em Operação</xMotivo><cUF>35</cUF><dhRecbto>2014-12-01T15:28:29-02:00</dhRecbto><tMed>1</tMed></retConsStatServ></nfeStatusServicoNF2Result></soap:Body></soap:Envelope>';
         $resp = $soap->send($urlservice, $namespace, $header, $body, $method);
         $this->assertEquals($resp, $respStd);
+    }
+    
+    /**
+     * @expectedException Common\Exception\RuntimeException
+     * @expectedExceptionMessage Não houve retorno do Curl.
+     */
+    public function testSendNaoHouveRetornoDoWebservice() 
+    {
+        $priKey = dirname(dirname(dirname(__FILE__))) . '/fixtures/certs/99999090910270_priKEY.pem';
+        $pubKey = dirname(dirname(dirname(__FILE__))) . '/fixtures/certs/99999090910270_pubKEY.pem';
+        $certKey = dirname(dirname(dirname(__FILE__))) . '/fixtures/certs/99999090910270_certKEY.pem';
+        $timeout = '10';
+        $args = array($priKey, $pubKey, $certKey, $timeout);
+        
+        $soap = $this->getMockBuilder('Common\Soap\CurlSoap')
+            ->setConstructorArgs($args)
+            ->setMethods(array('zCommCurl'))
+            ->getMock();
+
+        $soap->expects($this->any())
+                ->method('zCommCurl')
+                ->will($this->returnValue(false));
+        
+        $urlservice = 'https://homologacao.nfe.fazenda.sp.gov.br/ws/nfestatusservico2.asmx';
+        $namespace = "http://www.portalfiscal.inf.br/nfe/wsdl/NfeStatusServico2";
+        $header = '<nfeCabecMsg xmlns="http://www.portalfiscal.inf.br/nfe/wsdl/NfeStatusServico2"><cUF>35</cUF><versaoDados>3.10</versaoDados></nfeCabecMsg>';
+        $method = "nfeStatusServicoNF2";
+        $body = '<nfeDadosMsg xmlns="http://www.portalfiscal.inf.br/nfe/wsdl/NfeStatusServico2"><consStatServ xmlns="http://www.portalfiscal.inf.br/nfe" versao="3.10"><tpAmb>2</tpAmb><cUF>35</cUF><xServ>STATUS</xServ></consStatServ></nfeDadosMsg>';
+        $soap->send($urlservice, $namespace, $header, $body, $method);
     }
 }
