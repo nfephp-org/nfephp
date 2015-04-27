d3a83ce4cb5bb5c778029a8fbd1b2bb36ef63e44
robmachado <linux.rlm@gmail.com>
2015-04-27 13:03:28 -0300

    [UPD] Correção LotNumberTest.php

--- tests/Common/LotNumber/LotNumberTest.php
+++ tests/Common/LotNumber/LotNumberTest.php
@@ -5,7 +5,7 @@
  * @author Roberto L. Machado <linux dot rlm at gmail dot com>
  */
 
-class NFePHP\LotNumberTest extends PHPUnit_Framework_TestCase
+class LotNumberTest extends PHPUnit_Framework_TestCase
 {
    public function testGeraNumLote()
    {
