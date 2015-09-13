d8b8cbb1d85c61b3b09d7ddf07ae715ebff9eda3
jozephbrasil <jozephbrasil@gmail.com>
2015-09-13 12:38:16 -0300

    Layout de outras cidades
    Foi adicionado o layout de todas as capitais do Brasil.

--- libs/NFSe/Model/City.php
+++ libs/NFSe/Model/City.php
@@ -14,7 +14,33 @@ namespace NFSe\Model;
 abstract class City
 {
     // @todo colocar a associação Município x Layout em uma configuração
+    const ARACAJU = '2800308';
+    const BELEM = '1501402';
+    const BELO_HORIZONTE = '3106200';
+    const BOA_VISTA = '1400100';
+    const BRASILIA = '5300108';
+    const CAMPO_GRANDE = '5002704';
+    const CUIABA = '5103403';
+    const CURITIBA = '4106902';
+    const FLORIANOPOLIS = '4205407';
+    const FORTALEZA = '2304400';
+    const GOIANIA = '5208707';
+    const JOAO_PESSOA = '2507507';
+    const MACAPA = '1600303';
+    const MACEIO = '2704302';
+    const MANAUS = '1302603';
+    const NATAL = '2408102';
+    const PALMAS = '1721000';
+    const PORTO_ALEGRE = '4314902';
+    const PORTO_VELHO = '1100205';
+    const RECIFE = '2611606';
+    const RIO_BRANCO = '1200401';
+    const RIO_DE_JANEIRO = '3304557';
+    const SALVADOR = '2927408';
+    const SAO_LUIS = '2111300';
     const SAO_PAULO = '3550308';
+    const TERESINA = '2211001';
+    const VITORIA = '3205309';
 }
 
 ?>
