c7f788018c95d69d7b789f54dac23b4821cd6d8c
Marcos Diez <marcos@unitron.com.br>
2016-01-06 01:32:17 +0100

    apenas refatoracao. agora eu ate entendo o que se passa - parte2

--- libs/Extras/Danfe.php
+++ libs/Extras/Danfe.php
@@ -1837,8 +1837,8 @@ class Danfe extends CommonNFePHP implements DocumentoNFePHP
         $h = 7;
 
         $x = $this->pImpostoDanfeHelperV2($x, $y, $w, $h, "BASE DE CÁLCULO DO ICMS", "vBC");
-        $x = $this->pImpostoDanfeHelperV2($x, $y, $w, $h, "VALOR DO ICMS", "vICMS");
-        $x = $this->pImpostoDanfeHelperV2($x, $y, $w, $h, "BASE DE CÁLC. ICMS S.T.", "vBCST");
+        $x = $this->pImpostoDanfeHelperV2($x, $y, $wPis, $h, "VALOR DO ICMS", "vICMS");
+        $x = $this->pImpostoDanfeHelperV2($x, $y, $wPis, $h, "BASE DE CÁLC. ICMS S.T.", "vBCST");
         $x = $this->pImpostoDanfeHelperV2($x, $y, $w, $h, "VALOR DO ICMS SUBST.", "vST");
         $x = $this->pImpostoDanfeHelperV2($x, $y, $w, $h, "VALOR IMP. IMPORTAÇÃO", "vII");
         if ($this->exibirPIS) {
@@ -1855,8 +1855,8 @@ class Danfe extends CommonNFePHP implements DocumentoNFePHP
         $x = $oldX;
 
         $x = $this->pImpostoDanfeHelperV2($x, $y, $w, $h, "VALOR DO FRETE", "vFrete");
-        $x = $this->pImpostoDanfeHelperV2($x, $y, $w, $h, "VALOR DO SEGURO", "vSeg");
-        $x = $this->pImpostoDanfeHelperV2($x, $y, $w, $h, "DESCONTO", "vDesc");
+        $x = $this->pImpostoDanfeHelperV2($x, $y, $wPis, $h, "VALOR DO SEGURO", "vSeg");
+        $x = $this->pImpostoDanfeHelperV2($x, $y, $wPis, $h, "DESCONTO", "vDesc");
         $x = $this->pImpostoDanfeHelperV2($x, $y, $w, $h, "OUTRAS DESPESAS", "vOutro");
         $x = $this->pImpostoDanfeHelperV2($x, $y, $w, $h, "VALOR TOTAL DO IPI", "vIPI");
         if ($this->exibirPIS) {
