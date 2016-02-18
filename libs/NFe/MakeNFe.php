5ff19aad47529c2370c6867f112ac23158db9088
Roberto L. Machado <linux.rlm@gmail.com>
2016-02-18 16:28:37 -0200

    [UPD] MakeNFe.php - A exigencia do CNPJ 99999999000191 não existe mais

--- libs/NFe/MakeNFe.php
+++ libs/NFe/MakeNFe.php
@@ -788,7 +788,6 @@ class MakeNFe extends BaseMake
         }
         if ($this->tpAmb == '2' && $this->mod == '55') {
             $xNome = 'NF-E EMITIDA EM AMBIENTE DE HOMOLOGACAO - SEM VALOR FISCAL';
-            $cnpj = '99999999000191';
         }
         if ($cnpj != '') {
             $this->dom->addChild(
