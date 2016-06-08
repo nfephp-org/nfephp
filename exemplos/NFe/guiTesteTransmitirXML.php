0c485d21af4ac02210c8b91f0d3786a1ed40e44b
Jean Carlo Machado <contato@jeancarlomachado.com.br>
2016-06-08 10:28:06 -0300

    atualizar documentação de validação e transmissão da nota fiscal

--- exemplos/NFe/guiTesteTransmitirXML.php
+++ exemplos/NFe/guiTesteTransmitirXML.php
@@ -1,37 +1,40 @@
 <?php
 
-	header('Content-Type: text/html; charset=utf-8');
+header('Content-Type: text/html; charset=utf-8');
 
-	if(isset($_POST['conteudoArquivo']) || strlen(trim($_POST['conteudoArquivo'])) > 0){
+if(isset($_POST['conteudoArquivo']) || strlen(trim($_POST['conteudoArquivo'])) > 0){
 
-		require_once('../../libs/NFe/ToolsNFePHP.class.php');
-		$nfe = new ToolsNFePHP;
-		$modSOAP = '2'; //usando cURL
 
-		//obter um numero de lote
-		$lote = substr(str_replace(',', '', number_format(microtime(true)*1000000, 0)), 0, 15);
-		// montar o array com a NFe
-		$sNFe = $_POST['conteudoArquivo'];
-		//array vazio passado como referencia
-		$aResp = array();
+    //$config = configuracao da nfe
+    $nfe = new \NFePHP\NFe\ToolsNFe(json_encode($config));
 
-		//enviar o lote
-		if ($resp = $nfe->autoriza($sNFe, $lote, $aResp)) {
-		    if ($aResp['bStat']) {
-		        echo("Nota transmitida com sucesso! Utilize o número [" . $aResp['infRec']['nRec'] . "] do recibo para obter o protocolo ou informações de erro no xml com testaRecibo.php.");
-		    } else {
-		        echo('ERRO: ' . $nfe->errMsg);
-		    }
-		} else {
-		    echo('ERRO: ' . $nfe->errMsg);
-		}
-		echo('---------- BEGIN - SOAP DEBUG ----------');
-		echo($nfe->soapDebug);
-		echo('----------- END - SOAP DEBUG -----------');
+    $tpAmb = '2';
+    $aXml = $_POST['conteudoArquivo'];
+    $idLote = '';
+    $indSinc = '0';
+    $flagZip = false;
+    $aResposta;
 
-		die();
-	}
+    //array vazio passado como referencia
+    $aResp = array();
 
-	echo('ERRO: O conteúdo está vazio!');
+    //enviar o lote
+    if ($resp = $nfe->sefazEnviaLote( $aXml, $tpAmb, $idLote, $aResposta, $indSinc, $flagZip)) {
+        if ($aResp['bStat']) {
+            echo("Nota transmitida com sucesso! Utilize o número [" . $aResp['infRec']['nRec'] . "] do recibo para obter o protocolo ou informações de erro no xml com testaRecibo.php.");
+        } else {
+            echo('ERRO: ' . $nfe->errMsg);
+        }
+    } else {
+        echo('ERRO: ' . $nfe->errMsg);
+    }
+    echo('---------- BEGIN - SOAP DEBUG ----------');
+    echo($nfe->soapDebug);
+    echo('----------- END - SOAP DEBUG -----------');
+
+    die();
+}
+
+echo('ERRO: O conteúdo está vazio!');
 
 ?>
