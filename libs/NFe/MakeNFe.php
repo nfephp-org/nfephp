c7d4347c864d81ceb8c7e64fbb6bc734548195fd
Davi Conte <davi.j.conte@gmail.com>
2017-02-24 17:00:48 -0300

    Update MakeNFe.php - Gerar múltiplas tags med
    Estas alterações permitem que sejam geradas múltiplas tags de medicamento para cada produto.
    Trabalhando com medicamentos, percebeu-se a necessidade de gerar mais de uma tag com lote do medicamento, porém não estava implementado na biblioteca.
    Com as alterações implementadas, é criado um array para o produto, quando houver medicamentos, e neste array é possível gravar múltiplas tags <med> com suas propriedades chamando a função "nfe->tagmed()" em um laço de repetição ou quantas vezes forem necessárias.
    Obrigado.

--- libs/NFe/MakeNFe.php
+++ libs/NFe/MakeNFe.php
@@ -1795,7 +1795,7 @@ class MakeNFe extends BaseMake
         $this->dom->addChild($med, "dFab", $dFab, true, "$identificador [item $nItem] Data de fabricação");
         $this->dom->addChild($med, "dVal", $dVal, true, "$identificador [item $nItem] Data de validade");
         $this->dom->addChild($med, "vPMC", $vPMC, true, "$identificador [item $nItem] Preço máximo consumidor");
-        $this->aMed[$nItem] = $med;
+        $this->aMed[$nItem][] = $med;
         return $med;
     }
     
@@ -4096,9 +4096,11 @@ class MakeNFe extends BaseMake
         //insere medicamentos
         if (!empty($this->aMed)) {
             foreach ($this->aMed as $nItem => $child) {
-                $prod = $this->aProd[$nItem];
-                $this->dom->appChild($prod, $child, "Inclusão do node medicamento");
-                $this->aProd[$nItem] = $prod;
+				foreach($child as $grandChild){
+                	$prod = $this->aProd[$nItem];
+                	$this->dom->appChild($prod, $grandChild, "Inclusão do node medicamento");
+                	$this->aProd[$nItem] = $prod;
+				}
             }
         }
         //insere armas
